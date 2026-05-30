a4fdf4505d677d40a8db1cd0d617a624d5ffc062
imshahbaz21 <imshahbaz21@gmail.com>
2026-05-30 19:42:17 +0530

    fix

---
+++ app.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+const localAndroidGoogleServicesFile = path.join(__dirname, 'google-services.json');
+const localIosGoogleServicesFile = path.join(__dirname, 'GoogleService-Info.plist');
+
+module.exports = ({ config }) => ({
+  ...config,
+  ios: {
+    ...config.ios,
+    googleServicesFile:
+      process.env.GOOGLE_SERVICE_INFO_PLIST ?? config.ios?.googleServicesFile ?? localIosGoogleServicesFile,
+  },
+  android: {
+    ...config.android,
+    googleServicesFile:
+      process.env.GOOGLE_SERVICES_JSON ?? config.android?.googleServicesFile ?? localAndroidGoogleServicesFile,
+  },
+});
